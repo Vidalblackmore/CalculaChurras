ffe9c947481e0fc895f6260d25c558a8f7f87cd7
bcarneiro08@gmail.com <bcarneiro08@gmail.com>
2019-04-15 21:49:15 -0300

    Atualização Exercicio

---
+++ script.js
@@ -0,0 +1,20 @@
+function calcularQtd(){
+    var name "qtdhomem";
+    var name "qtdmulher";
+    var name "result";
+    
+}
+
+
+
+/* function calcularIdade() {
+    var idadeElement = window.document.getElementById("idade");
+    var idadeString = idadeElement.value;
+    console.log("idadeString = " + idadeString);
+    var idade = parseInt(idadeString);
+    if (idade >= 18) {
+        alert("MAIOR de idade.");
+    } else {
+        alert("MENOR de idade.");
+    }
+}*/
